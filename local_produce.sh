6fd4ed796a44d0f047c43224c3009f4dc4c93032
junzhou365 <junzhou365@gmail.com>
2015-11-05 04:32:13 +0000

    Change linux local configs

--- local_produce.sh
+++ local_produce.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-sed -i '12s/.*/public final static String imageFolerPath = \"\/home\/ubuntu\/public_images\/\";/' src/main/java/junzhou365/config/WebConfigurer.java
-sed -i '13s/.*/public final static String imageResourcePath = \"file:\/\/\/home\/ubuntu\/public_images\/\";/' src/main/java/junzhou365/config/WebConfigurer.java
-sed -i '14s/.*/public final static String CATALOG_ANGULARJS_LOCATION = \"file:\/\/\/home\/ubuntu\/webapp_catalog\/app\/\";/' src/main/java/junzhou365/config/WebConfigurer.java
-sed -i '4s/.*/server\.port=8080' src/main/resources/application.properties
+echo "Notice: need to kill process at first"
+sed -i '13s/.*/\tpublic final static String imageFolerPath = \"\/home\/vagrant\/public_images\/\";/' src/main/java/junzhou365/config/WebConfigurer.java
+sed -i '14s/.*/\tpublic final static String imageResourcePath = \"file:\/\/\/home\/vagrant\/public_images\/\";/' src/main/java/junzhou365/config/WebConfigurer.java
+sed -i '16s/.*/\tpublic final static String CATALOG_ANGULARJS_LOCATION = \"file:\/\/\/home\/vagrant\/webapp_catalog\/app\/\";/' src/main/java/junzhou365/config/WebConfigurer.java
+sed -i '3s/.*/server\.port=8000/' src/main/resources/application.properties
+mvn clean install
+sudo java -jar target/junzhou365-0.0.1-SNAPSHOT.jar &
