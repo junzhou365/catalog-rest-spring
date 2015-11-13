848b2a28f456ee2b696823e02b51ac23da74f5ce
junzhou365 <junzhou365@gmail.com>
2015-11-13 20:41:15 +0000

    Change produce.sh content

--- local_produce.sh
+++ local_produce.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 echo "Local"
-sed -i '13s/.*/\tpublic final static String imageFolerPath = \"\/home\/vagrant\/public_images\/\";/' src/main/java/junzhou365/config/WebConfigurer.java
-sed -i '14s/.*/\tpublic final static String imageResourcePath = \"file:\/\/\/home\/vagrant\/public_images\/\";/' src/main/java/junzhou365/config/WebConfigurer.java
-sed -i '15s/.*/\tpublic final static String SHAREDRESOURCES = \"file:\/\/\/home\/vagrant\/shared_resources\/\";/' src/main/java/junzhou365/config/WebConfigurer.java
-sed -i '16s/.*/\tpublic final static String CATALOG_ANGULARJS_LOCATION = \"file:\/\/\/home\/vagrant\/webapp_catalog\/app\/\";/' src/main/java/junzhou365/config/WebConfigurer.java
-sed -i '17s/.*/\tpublic final static String LIANPAGE_ANGULARJS_LOCATION = \"file:\/\/\/home\/vagrant\/lianpage\/app\/\";/' src/main/java/junzhou365/config/WebConfigurer.java
+sed -i '13s/.*/\tpublic final static String imageFolerPath = \"\/home\/vagrant\/public_images\/\";/' src/main/java/junzhou365/config/WebConfig.java
+sed -i '14s/.*/\tpublic final static String imageResourcePath = \"file:\/\/\/home\/vagrant\/public_images\/\";/' src/main/java/junzhou365/config/WebConfig.java
+sed -i '15s/.*/\tpublic final static String SHAREDRESOURCES = \"file:\/\/\/home\/vagrant\/shared_resources\/\";/' src/main/java/junzhou365/config/WebConfig.java
+sed -i '16s/.*/\tpublic final static String CATALOG_ANGULARJS_LOCATION = \"file:\/\/\/home\/vagrant\/webapp_catalog\/app\/\";/' src/main/java/junzhou365/config/WebConfig.java
+sed -i '17s/.*/\tpublic final static String LIANPAGE_ANGULARJS_LOCATION = \"file:\/\/\/home\/vagrant\/lianpage\/app\/\";/' src/main/java/junzhou365/config/WebConfig.java
+sed -i '18s/.*/\tpublic final static String FROGGER_LOCATION = \"file:\/\/\/home\/vagrant\/frogger\/\";/' src/main/java/junzhou365/config/WebConfig.java
 sed -i '3s/.*/server\.port=8000/' src/main/resources/application.properties
-mvn clean install
+sed -i '4s/.*/logging\.level\.junzhou365=INFO/' src/main/resources/application.properties
+sed -i '5s/.*/logging\.level\.org\.springframework\.web=INFO/' src/main/resources/application.properties
+mvn clean install -DskipTests
 sudo java -jar target/junzhou365-0.0.1-SNAPSHOT.jar 
--- produce.sh
+++ produce.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 echo "Notice: need to kill process at first"
-sed -i '12s/.*/\tpublic final static String imageFolerPath = \"\/home\/ubuntu\/public_images\/\";/' src/main/java/catalog/config/WebConfigurer.java
-sed -i '13s/.*/\tpublic final static String imageResourcePath = \"file:\/\/\/home\/ubuntu\/public_images\/\";/' src/main/java/catalog/config/WebConfigurer.java
-sed -i '14s/.*/\tpublic final static String CATALOG_ANGULARJS_LOCATION = \"file:\/\/\/home\/ubuntu\/webapp_catalog\/app\/\";/' src/main/java/catalog/config/WebConfigurer.java
-sed -i '3s/.*/server\.tomcat\.basedir=logs/' src/main/resources/application.properties
-sed -i '4s/.*/server\.port=80/' src/main/resources/application.properties
-mvn clean install
-sudo java -jar target/catalog-0.0.1-SNAPSHOT.jar &
-
+sed -i '13s/.*/\tpublic final static String imageFolerPath = \"\/home\/ubuntu\/public_images\/\";/' src/main/java/junzhou365/config/WebConfig.java
+sed -i '14s/.*/\tpublic final static String imageResourcePath = \"file:\/\/\/home\/ubuntu\/public_images\/\";/' src/main/java/junzhou365/config/WebConfig.java
+sed -i '15s/.*/\tpublic final static String SHAREDRESOURCES = \"file:\/\/\/home\/ubuntu\/shared_resources\/\";/' src/main/java/junzhou365/config/WebConfig.java
+sed -i '16s/.*/\tpublic final static String CATALOG_ANGULARJS_LOCATION = \"file:\/\/\/home\/ubuntu\/webapp_catalog\/app\/\";/' src/main/java/junzhou365/config/WebConfig.java
+sed -i '17s/.*/\tpublic final static String LIANPAGE_ANGULARJS_LOCATION = \"file:\/\/\/home\/ubuntu\/lianpage\/app\/\";/' src/main/java/junzhou365/config/WebConfig.java
+sed -i '18s/.*/\tpublic final static String FROGGER_LOCATION = \"file:\/\/\/home\/ubuntu\/frogger\/\";/' src/main/java/junzhou365/config/WebConfig.java
+sed -i '3s/.*/server\.port=80/' src/main/resources/application.properties
+sed -i '4s/.*/logging\.level\.junzhou365=INFO/' src/main/resources/application.properties
+sed -i '5s/.*/logging\.level\.org\.springframework\.web=INFO/' src/main/resources/application.properties
+mvn clean install -DskipTests
+sudo java -jar target/junzhou365-0.0.1-SNAPSHOT.jar&
